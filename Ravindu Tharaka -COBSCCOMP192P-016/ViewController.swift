0942846789f29c002a2d5f14e16f1a8bcf63c782
RavinduTharaka <44726946+RavinduTharaka@users.noreply.github.com>
2021-02-07 13:21:18 +0530

    Last commit

---
+++ Ravindu Tharaka -COBSCCOMP192P-016/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ravindu Tharaka -COBSCCOMP192P-016
+//
+//  Created by RavinduTharaka on 2/4/21.
+//  Copyright © 2021 RavinduTharaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
